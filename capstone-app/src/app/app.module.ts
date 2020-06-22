3dcdafb2321e924a89d93c32da8a4e9b453e7fdf
Billy Porter <wporter2@nd.edu>
2020-06-22 12:24:27 -0400

    Initial Angular setup with appengine and modules

---
+++ capstone-app/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { PhotosModule } from './modules/photos/photos.module';
+import { MapsModule } from './modules/maps/maps.module';
+import { HistoryModule } from './modules/history/history.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HistoryModule,
+    PhotosModule,
+    MapsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
