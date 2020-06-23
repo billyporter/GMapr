503bd20866a62954068ead5ba1ee62f4c056966e
Sarah <sarahhud@gmail.com>
2020-06-23 13:27:19 -0500

    Added service and component files

--- capstone-app/src/app/modules/history/history.module.ts
+++ capstone-app/src/app/modules/history/history.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WikiMediaComponent } from './wiki-media/wiki-media.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [WikiMediaComponent],
   imports: [
     CommonModule
   ]
 })
 export class HistoryModule { }
+
+
+//mat-card
+//mat-label
+//[(ngModel)]
---
+++ capstone-app/src/app/modules/history/wiki-media/wiki-media.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wiki-media',
+  templateUrl: './wiki-media.component.html',
+  styleUrls: ['./wiki-media.component.scss']
+})
+export class WikiMediaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
