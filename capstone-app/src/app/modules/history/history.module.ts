87733f47aeb2d3afdcb65a235b589eb28d90b126
Sarahhud <37464708+Sarahhud@users.noreply.github.com>
2020-06-24 16:53:19 -0500

    Update history.module.ts

--- capstone-app/src/app/modules/history/history.module.ts
+++ capstone-app/src/app/modules/history/history.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WikiMediaComponent } from './wiki-media/wiki-media.component';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ WikiMediaComponent ],
