0b42e5a3b3778cb374743e918d7e211e6e069677
Sarah <sarahhud@gmail.com>
2020-06-26 16:51:39 -0600

    Add more service and wiki api http module tests

--- capstone-app/src/app/app.component.spec.ts
+++ capstone-app/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('capstone-app app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain(fixture.componentInstance.title);
   });
 });
--- capstone-app/src/app/modules/history/wiki-media/wiki-media.component.ts
+++ capstone-app/src/app/modules/history/wiki-media/wiki-media.component.ts
@@ -10,8 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './wiki-media.component.html',
   styleUrls: ['./wiki-media.component.scss']
 })
-// TODO(sarahhud): Add testing to ensure http request is sent to the wikipedia api.
-// TODO(sarahhud): Add testing to ensure it displays location history.
+
 export class WikiMediaComponent implements OnInit, OnDestroy {
   wikiResult: WikiSearchResult;
   destroy$ = new Subject<void>();
@@ -20,7 +19,7 @@ export class WikiMediaComponent implements OnInit, OnDestroy {
   constructor(private wikiService: WikiResultsService) { }
 
   ngOnInit(): void {
-    this.getResults('Orlando, Florida');
+    this.getResults('Stillwater, Oklahoma');
   }
 
   ngOnDestroy(): void {
@@ -33,7 +32,6 @@ export class WikiMediaComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this.destroy$))
       .subscribe((result: WikiSearchResult) => {
       this.wikiResult = result;
-      console.log(result);
       if (this.wikiResult.parse.text) {
         this.body = this.fixString(result.parse.text["*"]);
         this.loading = false;
@@ -47,13 +45,17 @@ export class WikiMediaComponent implements OnInit, OnDestroy {
 
   fixString(text: string): string {
     let firstIndex = text.indexOf("<span class=\"mw-headline\" id=\"History\">History</span>", 0);
-    let firstPartOfString = text.substring(firstIndex, text.length);
-    let endIndex = firstPartOfString.indexOf("<h2>", 0);
-    let startIndex = firstPartOfString.indexOf('</h2>', 0);
-    let middleOfString = firstPartOfString.substring(startIndex, endIndex);
-    let finalString = middleOfString.split('href="/wiki').join('target="_blank" href="https://en.wikipedia.org/wiki');
-    finalString.split('title="Edit section: History">edit').join('>');
-    return finalString;
+    if(firstIndex != -1){
+      let firstPartOfString = text.substring(firstIndex, text.length);
+      let endIndex = firstPartOfString.indexOf("<h2>", 0);
+      let startIndex = firstPartOfString.indexOf('</h2>', 0);
+      let middleOfString = firstPartOfString.substring(startIndex, endIndex);
+      let finalString = middleOfString.split('href="/wiki').join('target="_blank" href="https://en.wikipedia.org/wiki');
+      finalString.split('title="Edit section: History">edit').join('>');
+      return finalString;
+    }
+    console.log("<p>The city page was found, but unfortunatley there was no history paragraph found!</p>");
+    return text;
   }
 
   setHtml(text: string) {
