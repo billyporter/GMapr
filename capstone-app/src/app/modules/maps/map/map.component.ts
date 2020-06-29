854118e65251fbbee1c6ab52e9009f918b17ac43
Sarah <sarahhud@gmail.com>
2020-06-29 17:57:11 -0500

    Added back a file I removed.

---
+++ capstone-app/src/app/modules/maps/map/map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent {
+  // these coordinates are for a location in Miami
+  location = new google.maps.LatLng(26.011760, -80.139050);
+}
