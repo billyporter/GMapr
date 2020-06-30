3ea4a5ec98777c31b029501220321f2af6a5bb22
Sarahhud <37464708+Sarahhud@users.noreply.github.com>
2020-06-30 17:37:46 -0500

    Update wiki-results.service.spec.ts

--- capstone-app/src/app/modules/history/wiki-results.service.spec.ts
+++ capstone-app/src/app/modules/history/wiki-results.service.spec.ts
@@ -7,11 +7,15 @@ const testWikiResult = {
     parse: {
       title: 'Stillwater, Oklahoma',
       text: {
-        '*' : 'The north-central region of Oklahoma became part of the United States with the Louisiana '
-        + 'Purchase in 1803. In 1832, author and traveler Washington Irving provided the first recorded '
-        + 'description of the area around Stillwater in his book A Tour on the Prairies. He wrote of “a '
-        + 'glorious prairie spreading out beneath the golden beams of an autumnal sun. The deep and frequent '
-        + 'traces of buffalo, showed it to be a one of their favorite grazing grounds.”9'
+        '*' : '</h2><p>The north-central region of Oklahoma became part of the United States'
+        + 'with the <a target=\"_blank\" href=\"https://en.wikipedia.org/wiki/Louisiana_Purchase\"'
+        + 'title=\"Louisiana Purchase\">Louisiana Purchase</a> in 1803. In 1832, author and traveler'
+        +  '<a target=\"_blank\" href=\"https://en.wikipedia.org/wiki/Washington_Irving\" title=\"Washington'
+        + 'Irving\">Washington Irving</a> provided the first recorded description of the area around'
+        + 'Stillwater in his book <i>A Tour on the Prairies</i>. He wrote of “a glorious prairie spreading'
+        + 'out beneath the golden beams of an autumnal sun. The deep and frequent traces of buffalo,'
+        + 'showed it to be a one of their favorite grazing grounds.”<sup id=\"cite_ref-9\" class=\"reference'
+        +'\"><a href=\"#cite_note-9\">&#91;9&#93;</a></sup></p>'
       }
     }
   }
