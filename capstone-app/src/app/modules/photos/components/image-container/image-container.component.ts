5e3025b59ac3707918acde2cd0acada455a9a50f
Billy Porter <wporter2@nd.edu>
2020-06-30 15:27:34 -0400

    Image container limits

--- capstone-app/src/app/modules/photos/components/image-container/image-container.component.ts
+++ capstone-app/src/app/modules/photos/components/image-container/image-container.component.ts
@@ -1,52 +1,67 @@
 import { Photo } from 'src/app/modules/photos/photo-template';
 import { PhotoFetcher } from 'src/app/modules/photos/services/photo-fetcher.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-image-container',
   templateUrl: './image-container.component.html',
   styleUrls: ['./image-container.component.scss'],
 })
-export class ImageContainerComponent implements OnInit {
-  photos: Photo[] = [];
+export class ImageContainerComponent implements OnInit, OnChanges  {
+  displayPhotos: Photo[] = [];
+  originalPhotos: Photo[] = [];
   query: string;
-  limit: number;
   errorMessage: string;
+  @Input() limit: number;
+  @Output() limitChange = new EventEmitter<number>();
 
   constructor(private photosService: PhotoFetcher) {}
-
   ngOnInit() {
+    this.limit = 10;
     this.getPhotos();
+  }
+
+  ngOnChanges() {
+    this.limitPhotos(this.limit);
   }
 
   getPhotos() {
     this.query = 'Boston';
-    this.limit = 8;
-    this.photosService.getPhotos(this.query, this.limit).subscribe((results) => {
-      for (const item of results.items) {
-        if (item) {
-          const photo = {
-            title: item.title,
-            link: item.link,
-            contextLink: item.image.contextLink,
-            height: item.image.height,
-            width: item.image.width,
-          };
-          this.photos.push(photo);
+    this.photosService.getPhotos(this.query, 10).subscribe(
+      (results) => {
+        for (const item in results.items) {
+          if (item) {
+            const photo = {
+              title: results.items[item].title,
+              link: results.items[item].link,
+              contextLink: results.items[item].image.contextLink,
+              height: results.items[item].image.height,
+              width: results.items[item].image.width,
+            };
+            this.originalPhotos.push(photo);
+          }
         }
-      }
-      if (this.photos.length === 0) {
-        this.errorMessage = '0 images found';
-      } else {
-        this.limitPhotos(this.limit);
-      }
-    }, (error) => {
+        if (this.originalPhotos.length === 0) {
+          this.errorMessage = '0 images found';
+        } else {
+          this.limitPhotos(this.limit);
+        }
+      },
+      (error) => {
         this.errorMessage = error;
         console.log(this.errorMessage);
-    });
+      }
+    );
   }
 
   limitPhotos(limit: number) {
-    this.photos = this.photos.slice(0, limit);
+    this.displayPhotos = this.originalPhotos.slice(0, limit);
+  }
+
+  removePhoto(index: number) {
+    this.limit -= 1;
+    this.limitChange.emit(this.limit);
+    this.originalPhotos.splice(index, 1);
   }
 }
+
