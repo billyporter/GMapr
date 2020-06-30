446971292bc9d01750fb8515c2937f261f559afa
Billy Porter <wporter2@nd.edu>
2020-06-30 15:33:53 -0400

    Sets up photo filter

--- capstone-app/src/app/modules/photos/components/photos-section/photos-section.component.ts
+++ capstone-app/src/app/modules/photos/components/photos-section/photos-section.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-photos-section',
   templateUrl: './photos-section.component.html',
   styleUrls: ['./photos-section.component.scss']
 })
-export class PhotosSectionComponent implements OnInit {
+export class PhotosSectionComponent {
+  limitControl = new FormControl(10, [Validators.min(1), Validators.max(10)]);
+  maxPhotos = 10;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  updateLimit(newLimit: number) {
+    this.maxPhotos = newLimit;
+    this.limitControl = new FormControl(newLimit);
   }
-
 }
