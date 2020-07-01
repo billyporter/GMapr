14a44138a4e7c17f9fe1ce299a8668bb41417256
smontana30 <smontana@knights.ucf.edu>
2020-07-01 15:52:45 -0400

    initial updates for map

--- capstone-app/src/app/modules/maps/map/map.component.ts
+++ capstone-app/src/app/modules/maps/map/map.component.ts
@@ -1,13 +1,83 @@
 /// <reference types="googlemaps" />
-import { Component } from '@angular/core';
+import { Component,  ViewChild, ElementRef } from '@angular/core';
+import {MapInfoWindow, MapMarker} from '@angular/google-maps';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
+
 export class MapComponent {
-  // these coordinates are for a location in Miami
-  location = new google.maps.LatLng(26.011760, -80.139050);
+  @ViewChild('searchBar') searchBar: ElementRef;
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow;
+
+  location!: google.maps.LatLng;
+  zoom = 13;
+  position!: google.maps.LatLngLiteral;
+  clickLocation: google.maps.LatLng[] = [];
+  markers: google.maps.LatLng[] = [];
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+     const input = this.searchBar.nativeElement;
+    
+     const autoComplete = new google.maps.places.Autocomplete(input,
+       {fields: ['geometry', 'name'], types: ['(cities)']});
+     autoComplete.addListener('place_changed', () => {
+       this.location = autoComplete.getPlace().geometry.location;
+     });
+    });
+  }
+
+  // update location using search button instead of input bar 
+  searchButton(barcation: String) {
+    const input = this.searchBar.nativeElement;
+    const autoComplete = new google.maps.places.Autocomplete(input,
+      {fields: ['geometry', 'name'], types: ['(cities)']});
+    autoComplete.addListener('place_changed', () => {
+      this.location = autoComplete.getPlace().geometry.location;
+    });
+  }
+
+  getCurrentOrSetLocation() {
+    if (!this.location) {
+      return new google.maps.LatLng(this.position);
+    }
+
+    return this.location;
+  }
+
+  ngOnInit() {
+    this.getCurrentLocation();
+    this.getCurrentOrSetLocation();
+  }
+
+  getCurrentLocation () {
+    navigator.geolocation.getCurrentPosition(position => {
+    this.position = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      }
+    })
+
+    return this.position;
+  }
+
+  addMarker(event: google.maps.MouseEvent) {
+    this.markers.push(event.latLng);
+  }
+
+  clickMarker(markerp: MapMarker) {
+    this.infoWindow.open(markerp);
+  }
+
+  removeMarker(marker: MapMarker) {
+    for (let i = 0; i < this.markers.length; i++) {
+      if (this.markers[i].equals(marker.getPosition())) {
+        marker.ngOnDestroy();
+      }
+    }
+  }
 }
 
--- capstone-app/src/app/modules/maps/maps.module.ts
+++ capstone-app/src/app/modules/maps/maps.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './map/map.component';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
   declarations: [MapComponent],
   imports: [
     CommonModule,
-    GoogleMapsModule
+    GoogleMapsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatAutocompleteModule
   ],
   exports: [MapComponent]
 })
