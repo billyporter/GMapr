3a7a2f82ca32bd64783c36905ca6fa89a4c2cb98
Billy Porter <wporter2@nd.edu>
2020-07-01 17:34:14 -0400

    Adds indentation

--- capstone-app/src/app/modules/history/services/wiki-search-handler.service.ts
+++ capstone-app/src/app/modules/history/services/wiki-search-handler.service.ts
@@ -4,14 +4,14 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WikiSearchHandler {
   private static LINK = 'https://www.googleapis.com/customsearch/v1?';
   private static API_KEY = 'key=AIzaSyDI9uo_xPRWlgF9bSWnJl1xSbeb394tgTo';
   private static SEARCH_ENGINE = 'cx=015569016282384541059:opag6em0spe';
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {}
 
   // TODO: Add interface
   getQueryString(query: string) {
@@ -19,9 +19,10 @@ export class WikiSearchHandler {
       `${WikiSearchHandler.LINK}${WikiSearchHandler.API_KEY}&` +
       `${WikiSearchHandler.SEARCH_ENGINE}&` +
       `q=${query.split(' ').join('+')}&num=${1}`;
-    return this.http.get(fullUrl)
-    .pipe(
-      map((data: any) => data.items[0].formattedUrl.match(/([^\/]+$)/g)[0])
-    );
+    return this.http
+      .get(fullUrl)
+      .pipe(
+        map((data: any) => data.items[0].formattedUrl.match(/([^\/]+$)/g)[0])
+      );
   }
 }
