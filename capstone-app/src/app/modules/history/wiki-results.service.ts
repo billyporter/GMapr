cdefb65b3c256a708268dd448a311e2d6a112e69
Billy Porter <wporter2@nd.edu>
2020-07-01 15:09:15 -0400

    Modifies wiki results service to be compatible with query handler

--- capstone-app/src/app/modules/history/wiki-results.service.ts
+++ capstone-app/src/app/modules/history/wiki-results.service.ts
@@ -1,19 +1,31 @@
+import { switchMap } from 'rxjs/operators';
+import { WikiSearchHandler } from './services/wiki-search-handler.service';
 import { Injectable } from '@angular/core';
 import { WikiSearchResult } from './WikiSearchTemplate';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class WikiResultsService {
-  constructor(private readonly http: HttpClient) {}
-  private static readonly URL_BEGINNING = 'https://en.wikipedia.org/w/api.php?origin=*&action=parse&page=';
+  constructor(
+    private readonly http: HttpClient,
+    private searchHandler: WikiSearchHandler
+  ) {}
+  private static readonly URL_BEGINNING =
+    'https://en.wikipedia.org/w/api.php?origin=*&action=parse&page=';
   private static readonly URL_END = '&format=json';
 
   search(query: string): Observable<WikiSearchResult> {
-    const url = WikiResultsService.URL_BEGINNING + query + WikiResultsService.URL_END;
-    return this.http.get<WikiSearchResult>(url);
+    return this.searchHandler
+      .getQueryString(query)
+      .pipe(
+        switchMap((result) =>
+          this.http.get<WikiSearchResult>(
+            `${WikiResultsService.URL_BEGINNING}${result}${WikiResultsService.URL_END}`
+          )
+        )
+      );
   }
 }
