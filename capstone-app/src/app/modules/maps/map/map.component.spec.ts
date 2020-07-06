0c3238ca909edc2732c7fad3059f482ac861ed5b
smontana30 <smontana@knights.ucf.edu>
2020-07-06 17:52:33 -0400

    test for map

--- capstone-app/src/app/modules/maps/map/map.component.spec.ts
+++ capstone-app/src/app/modules/maps/map/map.component.spec.ts
@@ -2,6 +2,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MapComponent } from './map.component';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { By } from '@angular/platform-browser';
 
 
 describe('MapComponent', () => {
@@ -18,16 +19,50 @@ describe('MapComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MapComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
 
   it('should create the app', () => {
     // const fixture = TestBed.createComponent(MapComponent);
     // const app = fixture.componentInstance;
     expect(fixture.componentInstance).toBeTruthy();
   });
+
+  it('test map marker click event', () => {
+    const fixture = TestBed.createComponent(MapComponent);
+    fixture.detectChanges();
+
+    const coordinates = new google.maps.LatLng({lat: 26.011760, lng:  -80.139050});
+
+    fixture.componentInstance.markers.push(coordinates);
+    fixture.detectChanges();
+
+    const markerDebug = fixture.debugElement.query(By.css('.marker'));
+    const markerComponent = markerDebug.componentInstance;
+
+    expect(markerComponent.getPosition()).toEqual(coordinates);
+  });
+
+  it('test multiple map marker click event', () => {
+    const fixture = TestBed.createComponent(MapComponent);
+    fixture.detectChanges();
+    let coordinates: google.maps.LatLng;
+    let lat = 26.011760;
+    let lng = -80.139050
+    for (let i = 0; i < 5; i++) {
+      coordinates = new google.maps.LatLng(lat, lng);
+      lat++;
+      lng++;
+      fixture.componentInstance.markers.push(coordinates);
+      fixture.detectChanges();
+    }
+
+    const markerDebug = fixture.debugElement.queryAll(By.css('.marker'));
+    const markerComponent = markerDebug.map(marker => marker.componentInstance.getPosition());
+
+    expect(markerComponent).toEqual(fixture.componentInstance.markers);
+  });
+
 
   it('sets center and zoom of the map', () => {
     const options = {center: {lat: 26.011760, lng:  -80.139050}, zoom: 13};
