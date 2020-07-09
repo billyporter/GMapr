0f01a16bc6f3cb8d86cf4c2659330117074d11c9
smontana30 <smontana@knights.ucf.edu>
2020-07-09 16:59:57 -0400

    the rest of the maps code with places library

--- capstone-app/src/app/modules/maps/map/map.component.spec.ts
+++ capstone-app/src/app/modules/maps/map/map.component.spec.ts
@@ -1,13 +1,11 @@
 /// <reference types="googlemaps" />
-import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MapComponent } from './map.component';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { By } from '@angular/platform-browser';
-import { EventEmitter } from '@angular/core';
 
-
+// TODO: fix flacky test issue
 describe('MapComponent', () => {
-  let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
 
   beforeEach(async(() => {
@@ -27,29 +25,102 @@ describe('MapComponent', () => {
     expect(fixture.componentInstance).toBeTruthy();
   });
 
-  it('test making multiple markers', () => {
+  it('test multiple map marker click event', () => {
     const fixture = TestBed.createComponent(MapComponent);
     fixture.detectChanges();
     let coordinates: google.maps.LatLng;
     let lat = 26.011760;
-    let lng = -80.139050
+    let lng = -80.139050;
     for (let i = 0; i < 5; i++) {
       coordinates = new google.maps.LatLng(lat, lng);
       lat++;
       lng++;
-      fixture.componentInstance.markers.push(coordinates);
+      fixture.componentInstance.markers.push({
+        position: coordinates,
+      });
     }
     fixture.detectChanges();
 
-    const markerDebug = fixture.debugElement.queryAll(By.css('.marker'));
-    const markerPositions = markerDebug.map(marker => marker.componentInstance.getPosition());
+    const markerDebug = fixture.debugElement.queryAll(By.css('.custom-marker'));
+    const markerComponent = markerDebug.map(marker => marker.componentInstance.getPosition());
 
-    expect(markerPositions).toEqual(fixture.componentInstance.markers);
+    expect(markerComponent).toEqual(fixture.componentInstance.markers.map(marker => marker.position));
   });
 
+  it('test delete button', () => {
+    const fixture = TestBed.createComponent(MapComponent);
+    fixture.detectChanges();
+    let coordinates: google.maps.LatLng;
+    let lat = 26.011760;
+    let lng = -80.139050;
+    for (let i = 0; i < 5; i++) {
+      coordinates = new google.maps.LatLng(lat, lng);
+      lat++;
+      lng++;
+      fixture.componentInstance.markers.push({
+        position: coordinates,
+      });
+    }
+    fixture.detectChanges();
 
+    const deleteButton = fixture.debugElement.query(By.css('.button'));
+    deleteButton.nativeElement.click();
+    fixture.detectChanges();
+
+    const markerDebug = fixture.debugElement.queryAll(By.css('.custom-marker'));
+    const markerComponent = markerDebug.map(marker => marker.componentInstance.getPosition());
+    expect(markerComponent.length).toEqual(0);
+  });
+
+  it('test nearby search auto generated markers', () => {
+    const fixture = TestBed.createComponent(MapComponent);
+    fixture.detectChanges();
+    //search Miami Gardens
+    const searchLocation = new google.maps.LatLng(25.9420377, -80.2456045);
+    const coordinates = [
+      {lat: 25.96723, lng: -80.266204},
+      {lat: 25.9420377, lng: -80.2456045},
+      {lat: 25.9499743, lng: -80.20790439999999},
+      {lat: 25.9774105, lng: -80.24748629999999},
+      {lat: 25.9098525, lng: -80.2687515}, 
+      {lat: 25.9544579, lng: -80.1978578},
+      {lat: 25.9383788, lng: -80.2509212},
+      {lat: 25.9636111, lng: -80.2530556},
+      {lat: 25.9438492, lng: -80.22714599999999},
+      {lat: 25.9424855, lng: -80.2543383},
+      {lat: 25.9326195, lng: -80.2835547},
+      {lat: 25.939876, lng: -80.287765},
+      {lat: 25.9749119, lng: -80.2128896}, 
+    ];
+
+    spyOn(fixture.componentInstance.nearSearch, 'nearbySearch').and.callFake((request, callback) => {
+      const results: google.maps.places.PlaceResult[] = coordinates.map(coordinate => {
+        return {
+          name: 'test place',
+          formatted_address: 'test address',
+          types: [],
+          geometry: {
+            location: new google.maps.LatLng(coordinate),
+            viewport: undefined,
+          },
+        };
+      });
+      callback(results, undefined, undefined);
+    });
+
+    fixture.componentInstance.location = searchLocation;
+    fixture.componentInstance.placesRequestFunc(searchLocation);
+
+    const searchMarkers = fixture.componentInstance.searchMarkers.map(marker => {
+      const position = marker.position as google.maps.LatLng;
+      return position.toJSON();
+    });
+
+    expect(searchMarkers).toEqual(coordinates);
+  });
+  
   it('sets center and zoom of the map', () => {
-    const options = {center: {lat: 26.011760, lng:  -80.139050}, zoom: 13};
+    const options = {center: {lat: 26.011760, lng: -80.139050}, zoom: 13};
 
     const fixture = TestBed.createComponent(MapComponent);
     fixture.componentInstance.location = new google.maps.LatLng(options.center);
@@ -59,6 +130,7 @@ describe('MapComponent', () => {
     const mapDebug = fixture.debugElement.query(By.css('.map'));
     const mapComp = mapDebug.componentInstance;
     fixture.detectChanges();
+    console.log(mapComp);
 
     expect(mapComp.getCenter().toJSON()).toEqual(options.center);
     expect(mapComp.getZoom()).toEqual(13);
--- capstone-app/src/app/modules/maps/map/map.component.ts
+++ capstone-app/src/app/modules/maps/map/map.component.ts
@@ -1,6 +1,6 @@
 /// <reference types="googlemaps" />
-import { Component,  ViewChild, ElementRef } from '@angular/core';
-import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { Component,  ViewChild, ElementRef, AfterViewInit, OnInit } from '@angular/core';
+import { MapInfoWindow, MapMarker, GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map',
@@ -8,15 +8,30 @@ import { MapInfoWindow, MapMarker } from '@angular/google-maps';
   styleUrls: ['./map.component.scss']
 })
 
-export class MapComponent {
+export class MapComponent implements AfterViewInit, OnInit{
   @ViewChild('searchBar') searchBar: ElementRef;
   @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow;
+  @ViewChild(GoogleMap) mapComponent: GoogleMap;
 
+  nearSearch?: google.maps.places.PlacesService;
   location?: google.maps.LatLng;
+  city: String = "Enter a city";
   zoom = 13;
+  markerData = new Map<string, string[]>();
+  searchMarkers: google.maps.MarkerOptions[] = [];
+  autoMark: google.maps.Marker;
   position?: google.maps.LatLngLiteral;
-  clickLocation: google.maps.LatLng[] = [];
-  markers: google.maps.LatLng[] = [];
+  markers: google.maps.MarkerOptions[] = [];
+  locationNameArray: String[] = [];
+  cityLocation: string;
+  geocoder = new google.maps.Geocoder;
+  activeMark: string;
+  testlocation: google.maps.LatLng = new google.maps.LatLng(26.011760, -80.139050);
+  placesRequest = {
+    location: this.testlocation,
+    radius: 5000,
+    type: 'tourist_attraction'
+  }
 
   ngOnInit() {
     this.getCurrentLocation();
@@ -27,47 +42,122 @@ export class MapComponent {
     setTimeout(() => {
       this.locationSearch();
     });
+    this.nearSearch = new google.maps.places.PlacesService(this.mapComponent._googleMap);
   }
 
   locationSearch() {
     const input = this.searchBar.nativeElement;
+    this.cityLocation = this.searchBar.nativeElement.value;
     const autoComplete = new google.maps.places.Autocomplete(input,
-      {fields: ['geometry', 'name'], types: ['(cities)']});
+      {fields: ['geometry', 'name', 'formatted_address'], types: ['(cities)']});
     autoComplete.addListener('place_changed', () => {
       this.location = autoComplete.getPlace().geometry.location;
+      this.cityLocation = autoComplete.getPlace().formatted_address;
+      this.placesRequestFunc(this.location);
     });
+  }
+
+  deleteAllMarks() {
+    this.searchMarkers = [];
+  }
+
+  placesRequestFunc(location: google.maps.LatLng) {
+    this.placesRequest.location = this.location;
+    this.nearSearch.nearbySearch(this.placesRequest, results => {
+      // resetting data
+      this.deleteAllMarks();
+      this.markerData.clear();
+      this.locationNameArray = [];
+      for ( const result of results) {
+        this.searchMarkers.push(this.createMarker(result))
+        this.locationNameArray.push(result.name);
+        this.markerData.set(result.name, result.types);
+      }
+    });
+  }
+
+  createMarker(result: google.maps.places.PlaceResult): google.maps.MarkerOptions {
+   return {
+    position: result.geometry.location,
+    title: result.name,
+    icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/pink-dot.png"
+    }
+   };
+  }
+
+  openInfoWindow(marker: MapMarker) {
+    this.activeMark = marker._marker.getTitle();
+     this.infoWindow.open(marker);
   }
 
   getCurrentOrSetLocation() {
     if (!this.location) {
-      return new google.maps.LatLng(this.position);
+      if (this.position) {
+        this.location = new google.maps.LatLng(this.position);
+      }
+      else {
+        // default location Bogota, Colombia
+        this.location = new google.maps.LatLng(4.7110, -74.0721);
+      }
     }
-    return this.location;
   }
 
   getCurrentLocation () {
     navigator.geolocation.getCurrentPosition(position => {
-    this.position = {
+      this.position = {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
-      }
-    })
+      };
+      this.location = new google.maps.LatLng(this.position);
+      this.getCurrentOrSetLocation();
+      this.geocoder.geocode({'location': this.position, }, function(results, status) {
+        if (status === 'OK') {
+          this.cityLocation = results[5].formatted_address;
+        }
+      });
+      this.placesRequestFunc(this.location);
+    }, () => {
+      // default position
+      this.position = {lat: 4.7110, lng: -74.0721};
+      this.getCurrentOrSetLocation();
+
+      this.geocoder.geocode({'location': this.position, }, function(results, status) {
+        if (status === 'OK') {
+          this.cityLocation = results[5].formatted_address;
+        }
+      });
+      this.cityLocation = 'Bogota, Colombia';
+      this.placesRequestFunc(this.location);
+    });
+    
+    return this.position;
   }
 
   addMarker(event: google.maps.MouseEvent) {
-    this.markers.push(event.latLng);
+    this.markers.push({ 
+         position: event.latLng,
+         title: 'Custom Marker',
+         icon: {
+           url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
+         }
+    });
   }
 
-  clickMarker(markerp: MapMarker) {
-    this.infoWindow.open(markerp);
+  clickMarker(marker: MapMarker) {
+    this.infoWindow.open(marker);
+  }
+
+  deleteAllCustomMark() {
+    this.markers = [];
   }
 
   removeMarker(marker: MapMarker) {
-    for (let i = 0; i < this.markers.length; i++) {
-      if (this.markers[i].equals(marker.getPosition())) {
-        marker.ngOnDestroy();
-      }
+    // position is guaranteed
+    const markerIndex = this.markers.map(currentMarker => currentMarker.position as google.maps.LatLng)
+      .indexOf(marker.getPosition());
+    if (markerIndex > -1) {
+      this.markers.splice(markerIndex, 1);
     }
   }
 }
-
