82bfc9baa5d3d2998f91413cf166a0d39224e34e
Billy Porter <wporter2@nd.edu>
2020-07-09 12:56:03 -0400

    button import for materials

--- capstone-app/src/app/modules/material/material.module.ts
+++ capstone-app/src/app/modules/material/material.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const material = [
   MatFormFieldModule,
   MatSelectModule,
   MatInputModule,
+  MatMenuModule,
+  MatButtonModule,
+  MatIconModule,
 ];
 
 @NgModule({
