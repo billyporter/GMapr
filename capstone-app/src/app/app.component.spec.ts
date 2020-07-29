7b66a680fefa15006f1c2630f2c346e3736846d3
Billy Porter <wporter2@nd.edu>
2020-07-29 17:42:19 -0400

    Testing for new flex container

--- capstone-app/src/app/app.component.spec.ts
+++ capstone-app/src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(fixture.componentInstance.title);
+    expect(compiled.querySelector('span').textContent).toContain(fixture.componentInstance.title[0]);
   });
 });
