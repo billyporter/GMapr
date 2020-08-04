c4bb57a756775801791f59bdb982748301e76d9e
Sofia Montana <sofiamontana@sofias-air-3.attlocal.net>
2020-08-03 22:04:24 -0400

    removed onboard component

--- capstone-app/src/app/modules/onboarding/onboarding.module.ts
+++ capstone-app/src/app/modules/onboarding/onboarding.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentComponent } from './content/content.component';
 import { MaterialModule } from '../material/material.module';
-import { OnboardComponent } from './onboard/onboard.component';
 
 @NgModule({
-  declarations: [ContentComponent, OnboardComponent],
+  declarations: [ContentComponent],
   imports: [
     CommonModule,
     MaterialModule,
